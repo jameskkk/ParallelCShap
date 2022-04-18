47af579fb37482e72293997132285a840a950e26
jameskkk <chenjhkk@gmail.com>
2022-04-18 10:11:51 +0800

    Add ParallelCShap into Github.

---
+++ ParallelCShap/ParallelCShap/FormMain.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ParallelCShap
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnColor2Grey = new System.Windows.Forms.Button();
+            this.btnColor2GreyTPL = new System.Windows.Forms.Button();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(25, 24);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(114, 64);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnColor2Grey
+            // 
+            this.btnColor2Grey.Location = new System.Drawing.Point(25, 105);
+            this.btnColor2Grey.Name = "btnColor2Grey";
+            this.btnColor2Grey.Size = new System.Drawing.Size(114, 64);
+            this.btnColor2Grey.TabIndex = 1;
+            this.btnColor2Grey.Text = "Color 2 Grey";
+            this.btnColor2Grey.UseVisualStyleBackColor = true;
+            this.btnColor2Grey.Click += new System.EventHandler(this.btnColor2Grey_Click);
+            // 
+            // btnColor2GreyTPL
+            // 
+            this.btnColor2GreyTPL.Location = new System.Drawing.Point(203, 105);
+            this.btnColor2GreyTPL.Name = "btnColor2GreyTPL";
+            this.btnColor2GreyTPL.Size = new System.Drawing.Size(142, 64);
+            this.btnColor2GreyTPL.TabIndex = 2;
+            this.btnColor2GreyTPL.Text = "Color 2 Grey TPL";
+            this.btnColor2GreyTPL.UseVisualStyleBackColor = true;
+            this.btnColor2GreyTPL.Click += new System.EventHandler(this.btnColor2GreyTPL_Click);
+            // 
+            // picResult
+            // 
+            this.picResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picResult.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.picResult.Location = new System.Drawing.Point(25, 192);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(320, 240);
+            this.picResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picResult.TabIndex = 5;
+            this.picResult.TabStop = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 444);
+            this.Controls.Add(this.picResult);
+            this.Controls.Add(this.btnColor2GreyTPL);
+            this.Controls.Add(this.btnColor2Grey);
+            this.Controls.Add(this.btnTest);
+            this.Name = "FormMain";
+            this.Text = "Parallel CShap";
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnColor2Grey;
+        private System.Windows.Forms.Button btnColor2GreyTPL;
+        private System.Windows.Forms.PictureBox picResult;
+    }
+}
+
---
+++ ParallelCShap/ParallelCShap/FormMain.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParallelCShap
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private string GetFixedLengthString(int len)
+        {
+            const string possibleAllChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+            StringBuilder sb = new StringBuilder();
+            Random randomNumber = new Random();
+            for (int i = 0; i < len; i++)
+            {
+                sb.Append(possibleAllChars[randomNumber.Next(0, possibleAllChars.Length)]);
+            }
+            return sb.ToString();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            // 產生測試資料
+            List<string> testData = new List<string>();
+
+            // 產生亂數字串
+            for (int i = 0; i < 100000; i++)
+            {
+                testData.Add(GetFixedLengthString(10));
+            }
+
+            ConcurrentStack<string> resultData = new ConcurrentStack<string>();
+
+            Parallel.ForEach(testData, (item, loopState) =>
+            {
+                if (item.Contains("a") || item.Contains("abc"))
+                {
+                    resultData.Push(item);
+                }
+            });
+
+            foreach (string data in resultData)
+            {
+                Console.WriteLine("data: " + data);
+            }
+        }
+
+        private void btnColor2Grey_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            string pathImage = Path.Combine(Directory.GetCurrentDirectory(), "CEO1.jpg");
+            picResult.Image = ConvertToGrey(pathImage);
+            stopWatch.Stop();
+            Console.WriteLine("Color2Grey() Time: " + stopWatch.ElapsedMilliseconds + " ms");
+        }
+
+        private void btnColor2GreyTPL_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopWatch = Stopwatch.StartNew();
+            string pathImage = Path.Combine(Directory.GetCurrentDirectory(), "CEO1.jpg");
+            picResult.Image = ConvertToGreyWithParallel(pathImage);
+            stopWatch.Stop();
+            Console.WriteLine("Color2GreyTPL() Time: " + stopWatch.ElapsedMilliseconds + " ms");
+        }
+
+        /// 
+        /// 轉換成灰階圖檔
+        /// 
+        public static Bitmap ConvertToGrey(string filePath)
+        {
+            // 來源圖檔
+            Bitmap bmp = new Bitmap(filePath);
+            // 建立新的Bitmap物件,用來存放轉換後的圖片
+            Bitmap newBmp = new Bitmap(bmp.Width, bmp.Height, bmp.PixelFormat);
+
+            BitmapData newData = newBmp.LockBits(new Rectangle(0, 0, newBmp.Width, newBmp.Height),
+                ImageLockMode.WriteOnly,
+                bmp.PixelFormat);
+
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, bmp.PixelFormat);
+
+            // 循序轉換
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    int offset = i * bmpData.Stride + j * (bmpData.Stride / bmpData.Width);
+                    // 直接在記憶體讀寫像素的RGB值
+                    int r = Marshal.ReadByte(bmpData.Scan0, offset + 2);
+                    int g = Marshal.ReadByte(bmpData.Scan0, offset + 1);
+                    int b = Marshal.ReadByte(bmpData.Scan0, offset);
+
+                    int grey = (r * 77 + g * 151 + b * 28) >> 8;
+                    Marshal.WriteByte(newData.Scan0, offset, (byte)grey);
+                    Marshal.WriteByte(newData.Scan0, offset + 1, (byte)grey);
+                    Marshal.WriteByte(newData.Scan0, offset + 2, (byte)grey);
+                }
+            }
+
+            newBmp.UnlockBits(newData);
+            bmp.UnlockBits(bmpData);
+
+            return newBmp;
+        }
+
+        ///
+        /// 透過TPL轉換成灰階圖檔
+        ///
+        public static Bitmap ConvertToGreyWithParallel(string filePath)
+        {
+            // 來源圖檔
+            Bitmap bmp = new Bitmap(filePath);
+            // 建立新的Bitmap物件,用來存放轉換後的圖片
+            Bitmap newBmp = new Bitmap(bmp.Width, bmp.Height, bmp.PixelFormat);
+
+            BitmapData newData = newBmp.LockBits(new Rectangle(0, 0, newBmp.Width, newBmp.Height), ImageLockMode.WriteOnly, bmp.PixelFormat);
+
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, bmp.PixelFormat);
+
+            int height = bmp.Height;
+            int width = bmp.Width;
+
+            // 平行轉換
+            Parallel.For(0, height, i =>
+                Parallel.For(0, width, j =>
+                {
+                    int offset = i * bmpData.Stride + j * (bmpData.Stride / bmpData.Width);
+                    // 直接在記憶體讀寫像素的RGB值
+                    int r = Marshal.ReadByte(bmpData.Scan0, offset + 2);
+                    int g = Marshal.ReadByte(bmpData.Scan0, offset + 1);
+                    int b = Marshal.ReadByte(bmpData.Scan0, offset);
+
+                    int grey = (r * 77 + g * 151 + b * 28) >> 8;
+                    Marshal.WriteByte(newData.Scan0, offset, (byte)grey);
+                    Marshal.WriteByte(newData.Scan0, offset + 1, (byte)grey);
+                    Marshal.WriteByte(newData.Scan0, offset + 2, (byte)grey);
+                }));
+
+
+            newBmp.UnlockBits(newData);
+            bmp.UnlockBits(bmpData);
+            return newBmp;
+        }
+    }
+}
